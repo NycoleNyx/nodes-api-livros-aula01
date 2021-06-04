c8f089de415c4399043c383086726e50fd47d233
reizinho1 <joaovitorlucas21@gmail.com>
2021-06-04 16:32:28 +0000

    Aula 02-api-livros

--- api-livros/src/index.js
+++ api-livros/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { uuid } =require('uuidv4');
+
 const app = express();
 
 app.use(express.json());
@@ -7,22 +9,62 @@ const livros = require("./livros.json");
 
 app.get('/livros', (request, response) => {
 
+    const { titulo } = request.query;
+
+    const resultado = titulo
+        ? livros.filter( livro => livro.titulo.includes(titulo) )
+        : livros
+
     return response.json( livros )
 });
 
 app.post('/livros', (request, response) => {
-    const { titulo, autor, anoPublicacao } = request.body;
-    return response.json( {titulo: titulo, autor: autor, ano: anoPublicacao} )
+
+    const { titulo, descricao, autor, anoPublicacao } = request.body;
+
+    const livro = { idd: uuid(), titulo, descricao, autor, anoPublicacao };
+
+    livros.push(livro)
+
+    return response.json( livro )
 });
 
 app.put('/livros/:id', (request, response) => {
-    const parametro = request.params;
-    return response.json( parametro )
+
+    const { id, titulo, descricao, autor, anoPublicacao } = request.body;
+
+    const indiceLivro = livros.findIndex( livro => livro.id === id );
+     
+    if( indiceLivro < 0) {
+        return response.status(400).json({ error: 'Livro não encontrado.' });
+    }
+
+    const livro = {
+        id,
+        titulo,
+        descricao,
+        autor,
+        anoPublicacao
+    }
+
+    livros[indiceLivro] = livro;
+
+    return response.json(livro);
 });
 
 app.delete('/livros/:id', (request, response) => {
+    
     const { id } = request.params;
-    return response.json( id )
+    
+    const indiceLivro = livros.findIndex( livro => livro.id === Number(id) );
+
+    if( indiceLivro < 0 ) {
+        return response.status(400({ error: 'Livro não encontrado.' }));
+    }
+
+    livros.splice(indiceLivro, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
